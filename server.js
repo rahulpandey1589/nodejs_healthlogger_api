01b1dfbaa2c495296b953ad340bbd00894d6c5ee
Rahul Pandey <rahulpandey862@gmail.com>
2021-09-30 09:43:14 +0530

    Basic setup of MongoDB Atlas

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 
 
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`server is running on port ${PORT}`)
+);
+
+// handle unhandled rejection
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Unhandled rejection: ${err.message}`);
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
