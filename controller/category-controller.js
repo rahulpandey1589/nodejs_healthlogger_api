65766e236e9c52aa08f0c40c8d71fbe7cf2d2da6
Rahul Pandey <rahul.pandey02@nagarro.com>
2021-09-30 19:38:00 +0530

    Added Category Master

--- controller/category-controller.js
+++ controller/category-controller.js
@@ -3,14 +3,11 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/asyncHandler");
 
 exports.addCategory = asyncHandler(async (req, res, next) => {
-
-    console.log(req.body);
-
     let response= await CategoryModel.create(req.body);
-
     res.status(200).json({
         success:true,
-        message:'Category Added'
+        message:'Category Added!!!',
+        data:response.id
     });
 });
 
--- controller/user-controller.js
+++ controller/user-controller.js
@@ -26,12 +26,11 @@ exports.authenticateUser = asyncHandler(async (req, res, next) => {
 });
 
 exports.createUser = asyncHandler(async (req, res, next) => {
-  console.log(req.body);
-
   const bootCampResponse = await UserModel.create(req.body);
 
   res.status(200).json({
     success: true,
     message: "User Created!!!",
+    data:bootCampResponse.id
   });
 });
