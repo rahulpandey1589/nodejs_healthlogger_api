836d0a3af4b0b106c3f7b20c73159f3f81d0f192
Rahul Pandey <rahulpandey862@gmail.com>
2021-10-01 10:20:37 +0530

    Find all categories and based on ID

--- controller/category-controller.js
+++ controller/category-controller.js
@@ -3,35 +3,43 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/asyncHandler");
 
 exports.addCategory = asyncHandler(async (req, res, next) => {
-    let response= await CategoryModel.create(req.body);
-    res.status(200).json({
-        success:true,
-        message:'Category Added!!!',
-        data:response.id
-    });
+  let response = await CategoryModel.create(req.body);
+  res.status(200).json({
+    success: true,
+    message: "Category Added!!!",
+    data: response.id,
+  });
 });
 
 exports.getAllCategory = asyncHandler(async (req, res, next) => {
-
+  let response = await CategoryModel.find();
+  if (response !== undefined) {
     res.status(200).json({
-        success:true,
-        message:'Category Added'
+      success: true,
+      message: "Categories retrieved",
+      data: response,
     });
+  }
 });
 
 exports.getCategoryById = asyncHandler(async (req, res, next) => {
+  let categoryId = req.params.id;
 
-    res.status(200).json({
-        success:true,
-        message:'Category Added'
-    });
+  if (categoryId !== undefined) {
+    let response = await CategoryModel.findById(categoryId);
+    if (response !== undefined) {
+      res.status(200).json({
+        success: true,
+        message: "Category Retreived",
+        data: response,
+      });
+    }
+  }
 });
 
-
 exports.deleteCategoryById = asyncHandler(async (req, res, next) => {
-
-    res.status(200).json({
-        success:true,
-        message:'Category Added'
-    });
+  res.status(200).json({
+    success: true,
+    message: "Category Added",
+  });
 });
