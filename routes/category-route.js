dd2e480e60e53cf61e975ceccb1b56ca3001585a
Rahul Pandey <rahul.pandey02@nagarro.com>
2021-10-01 17:30:58 +0530

    Protect route based on roles

--- routes/category-route.js
+++ routes/category-route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {protect} = require('../middleware/guard-middleware');
+const { protect, authorize } = require("../middleware/guard-middleware");
 
 const router = express.Router();
 
@@ -10,9 +10,14 @@ const {
   getCategoryById,
 } = require("../controller/category-controller");
 
-router.route("/").get(protect,getAllCategory).post(protect,addCategory);
+router
+  .route("/")
+  .get(protect, getAllCategory)
+  .post(protect, authorize("admin"), addCategory);
 
-router.route("/:id").get(protect,getCategoryById).delete(protect,deleteCategoryById);
-
+router
+  .route("/:id")
+  .get(protect, getCategoryById)
+  .delete(protect, authorize("admin"), deleteCategoryById);
 
 module.exports = router;
