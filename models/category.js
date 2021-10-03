c468688ab3bfb078d6ccde744fdebce5eb2e1dcb
Rahul Pandey <rahulpandey862@gmail.com>
2021-10-03 13:45:10 +0530

    Added createdAt in category

--- models/category.js
+++ models/category.js
@@ -1,20 +1,27 @@
 const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema({
-    category_name:{
-        type:String,
-        required:[true,'Category Name is required.'],
-        minlength:[5,'Category Name should be atleast five characters long'],
-        maxlength:[15,'Category Name should be maximum of 15 characters long'],
-        unique:true
-    },
-    description:{
-        type:String,
-        required:[true,'Description is required.'],
-        minlength:[5,'Description should be atleast five characters long'],
-        maxlength:[50,'Description should be maximum of 15 characters long']
-    },
-    isactive:Boolean
-})
+  category_name: {
+    type: String,
+    required: [true, "Category Name is required."],
+    minlength: [5, "Category Name should be atleast five characters long"],
+    maxlength: [15, "Category Name should be maximum of 15 characters long"],
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required."],
+    minlength: [5, "Description should be atleast five characters long"],
+    maxlength: [50, "Description should be maximum of 15 characters long"],
+  },
+  isactive: {
+    type: Boolean,
+    default: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports= mongoose.model("Category",CategorySchema);
+module.exports = mongoose.model("Category", CategorySchema);
