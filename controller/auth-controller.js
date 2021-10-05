a34f17f2e5245882ede679f4dd9136bfae92490d
Rahul Pandey <rahul.pandey02@nagarro.com>
2021-10-05 19:19:01 +0530

    Changes in auth controller.js

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -31,6 +31,7 @@ exports.authenticate = asyncHandler(async (req, res, next) => {
 
 
 exports.register = asyncHandler(async (req, res, next) => {
+  console.log(req.body);
   const user = await UserModel.create(req.body);
   res.status(200).json({
     success: true,
