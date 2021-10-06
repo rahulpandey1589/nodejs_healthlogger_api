58aaa9dced4a624a9830db07b7c9cede4a567473
Rahul Pandey <rahulpandey862@gmail.com>
2021-10-06 08:59:17 +0530

    Auth Controller

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -27,13 +27,13 @@ exports.authenticate = asyncHandler(async (req, res, next) => {
 });
 
 exports.register = asyncHandler(async (req, res, next) => {
-  const errors = validationResult(req);
 
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json({ errors: errors.array() });
     return;
   }
-  console.log(req.body);
+
   const user = await UserModel.create(req.body);
   res.status(200).json({
     success: true,
