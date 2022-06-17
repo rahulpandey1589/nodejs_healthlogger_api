e7d44266f7cfaafd1070cb369e543b3e3dfdf19c
Rahul Pandey <rahul.pandey02@nagarro.com>
2022-06-17 15:16:12 +0530

    Changes related to docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM  node:alpine
 
 LABEL author="Rahul Pandey"
-
 ENV NODE_ENV=production
 ENV PORT=1158
+ENV MONGO_URI='mongodb://mongo:27017/health_logger?retryWrites=true&w=majority'
 
 WORKDIR /var/www
 COPY package.json package-lock.json ./
--- config/db.js
+++ config/db.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 
 const connectDB = async() =>{
-    const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log(`The mongo URI is ,${process.env.MONGO_URI} and port number is ${process.env.PORT}`)
+    const conn = await mongoose.connect(
+        process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log(`MongoDB Connected, ${conn.connection.host}`);
+
 }
 
 module.exports = connectDB;
