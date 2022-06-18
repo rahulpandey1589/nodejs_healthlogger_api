727874449165ee8b1c34bc0420fa7c2f0ee84d05
Rahul Pandey <rahul.pandey02@nagarro.com>
2022-06-18 09:44:45 +0530

    Changes in env and db files

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 
 const connectDB = async() =>{
-    console.log(`The mongo URI is ,${process.env.MONGO_URI} and port number is ${process.env.PORT}`)
+    console.log(`The mongo URI is ,${process.env.MONGO_URI} and port number is ${process.env.PORT}
+                and Test port is ${process.env.TEST_ENV_PORT}`)
     const conn = await mongoose.connect(
         process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log(`MongoDB Connected, ${conn.connection.host}`);
